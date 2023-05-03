db98feec4ab780f532497554f6a8afb917454102
Siaw Henry Nartey <henrysiaw159@gmail.com>
2023-05-03 16:30:12 +0000

    code recheck

--- dist/game.js
+++ dist/game.js
@@ -4,44 +4,6 @@ const typingGame = {
   textAreaElement: document.getElementById("text-area-element"),
   quoteElementDisplay: document.getElementById("quote-element-display"),
   timerElement: document.getElementById("get-ready-element"),
-  quotes: {
-    1: {
-      text: "The quick brown fox jumped over the fence!",
-    },
-    2: {
-      text: "This is the shortest quote possible, one hundred and fifty characters, no colons, no parenthesis, that is it, that is the entire quote, have a great day.",
-    },
-    3: {
-      text: "Here is one quick and easy typing prompt for you. Short and speedy.. You got this! What is your favorite Pokemon? Mine is Mudkip. Mudkip solos. 1 2 3.. are you ready? Mudkip Mudkip Mudkip Mudkip.",
-    },
-    4: {
-      text: "This is an easy quote to type. Should be short and concise for you to type it faster than usual and should have short enough words for you to speed up.",
-    },
-    5: {
-      text: "Fasting for just three days has shown a record increase in the body's HGH levels, about 300%. Intermittent fasting provides a good balance between eating and fasting cycles in the human body. This balance works towards a positive influence on HGH secretions, both in the short and long term.",
-    },
-    6: {
-      text: "You only live once, but if you do it right, once is enough.",
-    },
-    7: {
-      text: "Many of life’s failures are people who did not realize how close they were to success when they gave up.",
-    },
-    8: {
-      text: "Never let the fear of striking out keep you from playing the game.",
-    },
-    9: {
-      text: "My father, who was from a wealthy family and highly educated, a lawyer, Yale and Columbia, walked out with the benefit of a healthy push from my mother, a seventh grade graduate, who took a typing course and got a secretarial job as fast as she could.",
-    },
-    10: {
-      text: "Dynamic typing is not necessarily good. You get static errors at run time, which you really should be able to catch at compile time.",
-    },
-    11: {
-      text: "Over the years, I've trained myself to speak using the same language I would use if I were typing: meaning using full sentences in the way that paragraphs and scenes are arranged.",
-    },
-    12: {
-      text: "",
-    }
-  },
   getRandQuote: function () {
     let len = Object.keys(this.quotes).length;
     let decimal = Math.random() * len;
@@ -320,9 +282,162 @@ const typingGame = {
       this.countElementBox.style.opacity = 1;
       this.countElementBox.style.visibility = "hidden";
     })
-    
+
     // Input terminate
     this.textAreaElement.removeEventListener("input", this.checkInputBind);
+  },
+  quotes: {
+    1: {
+      text: "The quick brown fox jumped over the fence!",
+    },
+    2: {
+      text: "This is the shortest quote possible, one hundred and fifty characters, no colons, no parenthesis, that is it, that is the entire quote, have a great day.",
+    },
+    3: {
+      text: "Here is one quick and easy typing prompt for you. Short and speedy.. You got this! What is your favorite Pokemon? Mine is Mudkip. Mudkip solos. 1 2 3.. are you ready? Mudkip Mudkip Mudkip Mudkip.",
+    },
+    4: {
+      text: "This is an easy quote to type. Should be short and concise for you to type it faster than usual and should have short enough words for you to speed up.",
+    },
+    5: {
+      text: "Fasting for just three days has shown a record increase in the body's HGH levels, about 300%. Intermittent fasting provides a good balance between eating and fasting cycles in the human body. This balance works towards a positive influence on HGH secretions, both in the short and long term.",
+    },
+    6: {
+      text: "You only live once, but if you do it right, once is enough.",
+    },
+    7: {
+      text: "Many of life’s failures are people who did not realize how close they were to success when they gave up.",
+    },
+    8: {
+      text: "Never let the fear of striking out keep you from playing the game.",
+    },
+    9: {
+      text: "My father, who was from a wealthy family and highly educated, a lawyer, Yale and Columbia, walked out with the benefit of a healthy push from my mother, a seventh grade graduate, who took a typing course and got a secretarial job as fast as she could.",
+    },
+    10: {
+      text: "Dynamic typing is not necessarily good. You get static errors at run time, which you really should be able to catch at compile time.",
+    },
+    11: {
+      text: "Over the years, I've trained myself to speak using the same language I would use if I were typing: meaning using full sentences in the way that paragraphs and scenes are arranged.",
+    },
+    12: {
+      text: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+      },
+      13: {
+      text: "The best way to predict the future is to invent it.",
+      },
+      14: {
+      text: "Happiness is not something ready made. It comes from your own actions.",
+      },
+      15: {
+      text: "The only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it.",
+      },
+      16: {
+      text: "The true test of character is not how much we know how to do, but how we behave when we don't know what to do.",
+      },
+      17: {
+      text: "Believe you can and you're halfway there.",
+      },
+      18: {
+      text: "The only person you are destined to become is the person you decide to be.",
+      },
+      19: {
+      text: "Don't watch the clock; do what it does. Keep going.",
+      },
+      20: {
+      text: "The best preparation for tomorrow is doing your best today.",
+      },
+      21: {
+      text: "Be the change you wish to see in the world.",
+      },
+      22: {
+      text: "You miss 100% of the shots you don't take.",
+      },
+      23: {
+      text: "I can't change the direction of the wind, but I can adjust my sails to always reach my destination.",
+      },
+      24: {
+      text: "The difference between ordinary and extraordinary is that little extra.",
+      },
+      25: {
+      text: "It does not matter how slowly you go as long as you do not stop.",
+      },
+      26: {
+      text: "It always seems impossible until it's done.",
+      },
+      27: {
+      text: "Don't let yesterday take up too much of today.",
+      },
+      28: {
+      text: "If you want to achieve greatness, stop asking for permission.",
+      },
+      29: {
+      text: "The future belongs to those who believe in the beauty of their dreams.",
+      },
+      30: {
+      text: "If you're going through hell, keep going.",
+      },
+      31: {
+      text: "If you want to lift yourself up, lift up someone else.",
+      },
+      32: {
+      text: "The way to get started is to quit talking and begin doing.",
+      },
+      33: {
+      text: "You are never too old to set another goal or to dream a new dream.",
+      },
+      34: {
+      text: "Success usually comes to those who are too busy to be looking for it.",
+      },
+      35: {
+      text: "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
+      },
+      36: {
+      text: "Opportunities are usually disguised as hard work, so most people don't recognize them.",
+      },
+      37: {
+      text: "Twenty years from now you will be more disappointed by the things you didn't do than by the ones you did do.",
+      },
+      38: {
+      text: "Great things never came from comfort zones.",
+      },
+      39: {
+      text: "Our greatest glory is not in never falling, but in rising every time we fall.",
+      },
+      40: {
+        text: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+        },
+        41: {
+        text: "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
+        },
+        42: {
+        text: "The only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it.",
+        },
+        43: {
+        text: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        },
+        44: {
+        text: "Education is the most powerful weapon which you can use to change the world.",
+        },
+        45: {
+        text: "Happiness is not something ready made. It comes from your own actions.",
+        },
+        46: {
+        text: "The best way to predict the future is to create it.",
+        },
+        47: {
+        text: "The way to get started is to quit talking and begin doing.",
+        },
+        48: {
+        text: "You miss 100% of the shots you don't take.",
+        },
+        49: {
+        text: "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
+        },
+        50: {
+        text: "The only limit to our realization of tomorrow will be our doubts of today.",
+        },
+
   },
 };
 
