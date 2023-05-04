6b41f53b2faa9ef887b4b3d6677355262203fa93
Siaw Henry Nartey <henrysiaw159@gmail.com>
2023-05-04 07:21:06 +0000

    second deployment

---
+++ dist/script.js
@@ -0,0 +1,10 @@
+// // Make element hidden at loading times
+// document.getElementById("toggle_hide_show").style.visibility = "hidden";
+
+function hide(){
+    document.getElementById("toggle_hide_show").style.visibility = "hidden";
+}
+
+function show(){
+    document.getElementById("toggle_hide_show").style.visibility = "visible";
+}
